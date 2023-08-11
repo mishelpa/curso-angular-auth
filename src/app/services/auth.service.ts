da8ad382eae9ca90fff2bfd03932b61fe5ee3c5e
Mishel Paredes <Mishel.Paredes@pe.ey.com>
2023-08-11 12:35:25 -0500

    Combinar register and login con switchmap

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
+import { switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,6 +23,14 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/api/v1/auth/register`, {
       name, email, password
     })
+  }
+
+  registerAndLogin(name: string, email: string, password: string) {
+    return this.register(name, email, password)
+    .pipe(
+      switchMap(() => this.login(email, password))
+    )
+
   }
 
   isAvailable(email: string) {
